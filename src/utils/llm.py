d85ce0eef90fec32ef919049ede9fd4dc33c2409
Mo <mohamedabdelhamid3030@gmail.com>
2025-05-01 17:31:23 +0300

    task: added support for openrouter api key

--- src/utils/llm.py
+++ src/utils/llm.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.language_models.chat_models import BaseChatModel
@@ -8,11 +9,19 @@ GPT_4O_MINI = "gpt-4o-mini"
 GPT_4O = "gpt-4o"
 GPT_4_1_MINI = "gpt-4.1-mini"
 
+params = {}
+if "OPEN_ROUTER_API_KEY" in os.environ:
+    params = {
+        "openai_api_key": os.environ["OPEN_ROUTER_API_KEY"],
+        "openai_api_base": "https://openrouter.ai/api/v1",
+    }
+else:
+    params = {}
 
 llms = {
-    GPT_4O_MINI: ChatOpenAI(model_name="gpt-4o-mini", temperature=0),
-    GPT_4O: ChatOpenAI(model_name="gpt-4o", temperature=0, top_p=0),
-    GPT_4_1_MINI: ChatOpenAI(model_name="gpt-4.1-mini", temperature=0),
+    GPT_4O_MINI: ChatOpenAI(model_name="gpt-4o-mini", temperature=0, **params),
+    GPT_4O: ChatOpenAI(model_name="gpt-4o", temperature=0, top_p=0, **params),
+    GPT_4_1_MINI: ChatOpenAI(model_name="gpt-4.1-mini", temperature=0, **params),
 }
 
 
